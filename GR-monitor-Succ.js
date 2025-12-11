d9d760abb2ce8a865db3c1d0e7dba17ef567c0b7
Li Chang <lichang.android@gmail.com>
2025-12-11 16:29:47 +0800

    Update and rename jd-efx8-monitor.js to GR-monitor-Succ.js

---
+++ GR-monitor-Succ.js
@@ -0,0 +1,29 @@
+/********* Ricoh GR III / IIIx 监控脚本 *********/
+
+const url = "https://newsite.ricn-mall.com/goods_cate?cid=9";
+
+$task.fetch({ url }).then(resp => {
+    let body = resp.body || "";
+
+    // 判断关键词是否出现
+    const foundIII = body.includes("III");
+    const foundIIIX = body.includes("IIIx") || body.includes("III X") || body.includes("GR IIIx");
+
+    let msg = "";
+
+    if (foundIII || foundIIIX) {
+        msg = "🚨 Ricoh GR 发现库存！";
+
+        if (foundIII) msg += "\n- GR III 出现";
+        if (foundIIIX) msg += "\n- GR IIIx 出现";
+
+        // QuantumultX 通知
+        $notify("🎉 GR 商品监控", "页面检测到目标型号", msg);
+    }
+    
+    $done({});
+}).catch(err => {
+    // 请求失败也发送通知
+    $notify("❌ GR 页面监控失败", "", err);
+    $done({});
+});
